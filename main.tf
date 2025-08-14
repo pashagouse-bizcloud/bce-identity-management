f9b5afcb5b82055202b31fc13cdffde32313de75
pashagouse-bizcloud <pasha.gouse@bizcloudexperts.com>
2025-08-14 14:34:38 +0530

    Fix GitHub Actions permissions: Add S3 and DynamoDB access for Terraform backend

--- main.tf
+++ main.tf
@@ -115,3 +115,37 @@ resource "aws_iam_role_policy_attachment" "github_actions_policy" {
   role       = aws_iam_role.github_actions.name
   policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
 }
+
+# Add S3 and DynamoDB permissions for Terraform state backend
+resource "aws_iam_role_policy" "github_actions_terraform_backend" {
+  name = "TerraformBackendAccess"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::bce-identity-management-terraform-state",
+          "arn:aws:s3:::bce-identity-management-terraform-state/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem"
+        ]
+        Resource = "arn:aws:dynamodb:us-east-1:*:table/terraform-state-lock"
+      }
+    ]
+  })
+}
